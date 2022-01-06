a1b38799293edddfd431c9f7a5cccadb0c4b0aa6
Hector Nagao <hectornagao@hotmail.com>
2022-01-06 13:34:55 -0300

    criado rotas e corrigido model de desenvolvedores

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\Niveis;
 
 class LevelController extends Controller
 {
@@ -13,7 +14,7 @@ class LevelController extends Controller
      */
     public function index()
     {
-        //
+        return Niveis::all();
     }
 
     /**
--- app/Models/Desenvolvedores.php
+++ app/Models/Desenvolvedores.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Desenvoledores extends Model
+class Desenvolvedores extends Model
 {
     use HasFactory;
     protected $table = 'desenvolvedores';
